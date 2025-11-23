e0579f8c7d5686a81e9ddf053824e84469aef1a7
lui5gl <lui5gl@outlook.com>
2025-11-23 17:00:02 -0600

    fix: add IconBulb import to Dashboard component

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -14,6 +14,7 @@ import { Label } from '@/components/ui/label';
 import { Spinner } from '@/components/ui/spinner';
 import { Form, Head, router } from '@inertiajs/vue3';
 import {
+    IconBulb,
     IconCopy,
     IconDotsVertical,
     IconMapPin,
